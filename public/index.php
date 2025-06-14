d5371a7a31580cc9ec8f62604d9d4f7157a08736
Lucas Safeno <lugnorio@gmail.com>
2025-06-14 01:44:29 -0300

    📁 ActiveRecord PHP: Estrutura e Autoload - Aula 01

--- public/index.php
+++ public/index.php
@@ -1,15 +1,2 @@
 <?php
 require '../vendor/autoload.php';
-
-use app\database\models\User;
-use app\database\activerecord\Update;
-use app\database\activerecord\UpdateUser;
-
-$user = new User;
-$user->firstName = 'Lucas';
-$user->lastName = 'Tenório';
-$user->id = 1;
-
-$user->update(updateInterface: new Update);
-
-// var_dump($user);
